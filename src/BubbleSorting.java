e7fbfed79a5d1f0a679644ddeea625c9424a426b
Spaik713 <spaik713@gmail.com>
2015-09-07 20:56:13 +0300

    Добавил пустую реализацию пузырьковой сортировки

---
+++ src/BubbleSorting.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class BubbleSorting implements SortingAlgorithm {
+
+	@Override
+	public ArrayList<String> sort(ArrayList<String> listToSort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/SortingAlgorithm.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public interface SortingAlgorithm {
+	
+	ArrayList<String> sort(ArrayList<String> listToSort);
+	
+}

f2bbe7c921f50d80071fb2ea46a2d7e2e1678976
Spaik713 <spaik713@gmail.com>
2015-09-08 23:03:20 +0300

    Bubble Sorting

--- src/BubbleSorting.java
+++ src/BubbleSorting.java
@@ -3,7 +3,21 @@ public class BubbleSorting implements SortingAlgorithm {
 
 	@Override
 	public String[] sort(String[] listToSort) {
-		return listToSort;
+		String[] reversedList = listToSort;
+		for (int counterOne=0; counterOne<reversedList.length-1; counterOne++){
+			
+			for (int counterTwo=0; counterTwo < reversedList.length-counterOne-1; counterTwo++){
+				
+				if (reversedList[counterTwo].compareTo(reversedList[counterTwo+1]) > 0){
+					
+					String buffer = reversedList[counterTwo];
+					reversedList[counterTwo] = reversedList[counterTwo+1];
+					reversedList[counterTwo+1] = buffer;
+				}
+			}
+		}
+		
+		return reversedList;
 	}
 	
 }
--- src/QuickSorting.java
+++ src/QuickSorting.java
@@ -3,6 +3,9 @@ public class QuickSorting implements SortingAlgorithm {
 
 	@Override
 	public String[] sort(String[] listToSort) {
+		
+		
+		
 		return null;
 	}
 
